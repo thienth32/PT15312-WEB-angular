7013055f56325023bf934749b3dd6775ce4ef7ef
thienth32 <thienth3@fe.edu.vn>
2021-03-22 09:03:20 +0700

    update routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeroDetailComponent } from './screens/hero-detail/hero-detail.component';
+import { HeroListComponent } from './screens/hero-list/hero-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HeroListComponent
+  },
+  {
+    path: 'monsters',
+    component: HeroDetailComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
