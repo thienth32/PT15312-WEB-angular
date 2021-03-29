6bda1cf8c3af3d7bbaca3110e064f4fd2c681506
thienth32 <thienth3@fe.edu.vn>
2021-03-29 08:13:27 +0700

    update home component

--- src/app/screens/home/home.component.ts
+++ src/app/screens/home/home.component.ts
@@ -6,7 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  counter(i: number) {
+    return new Array(i);
+  }
   constructor() { }
 
   ngOnInit(): void {
